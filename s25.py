156bc2a6d5798bb0a813816bd3d799d0431b0d30
carvalho009 <gustavo.gcj09@gmail.com>
2025-10-08 08:11:00 -0300

    25-1.py

--- s25.py
+++ s25.py
@@ -1,5 +1,22 @@
-Estoque = [20, 15, 10, 30, 5]
+estoque = [20, 15, 10, 30, 5]
 def atualizar(elemento, quantidade):
-    posicao = elemento - 1
-    Estoque[posicao]-= quantidade
+    posiçao = elemento - 1
+    if 0 <= posiçao < len(estoque):
+        estoque[posiçao]-= quantidade
+    else:
+        print("Elemento inválido!")
     
+def exibir (estoque):
+    print(f"Estoque atual :{estoque}")
+
+def adicionar(elemento, quatidade):
+    posiçao = elemento - 1
+    if 0<= posiçao < len(estoque):
+        estoque[posiçao]+= quatidade
+    else:
+        print("Elemento Inválido")
+exibir(estoque)
+atualizar(1,3 )        
+print("estoque atualizado")
+exibir(estoque)
+        
